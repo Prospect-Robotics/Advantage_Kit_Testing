23ca96e43a97f0d0b4d0a8740893ae6f903b044b
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2025-11-15 18:30:37 -0800

    Configured simulation FeedForward.

--- src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOSim.java
@@ -33,10 +33,11 @@ public class ModuleIOSim implements ModuleIO {
   // TunerConstants doesn't support separate sim constants, so they are declared locally
   private static final double DRIVE_KP = 0.05;
   private static final double DRIVE_KD = 0.0;
-  private static final double DRIVE_KS = 0.0;
+  private static final double DRIVE_KS = 0.00467;
   private static final double DRIVE_KV_ROT =
-      0.91035; // Same units as TunerConstants: (volt * secs) / rotation
+      0.83397; // Same units as TunerConstants: (volt * secs) / rotation
   private static final double DRIVE_KV = 1.0 / Units.rotationsToRadians(1.0 / DRIVE_KV_ROT);
+  // NOTE: Might wanna set this to 0.83397, and set the one before to 0.91035.
   private static final double TURN_KP = 8.0;
   private static final double TURN_KD = 0.0;
   private static final DCMotor DRIVE_GEARBOX = DCMotor.getKrakenX60Foc(1);
