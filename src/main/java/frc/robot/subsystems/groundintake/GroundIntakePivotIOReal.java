2cb7ae42e57a55dc16cb23c1aa37451d656d9a58
anonymes-axolotole <146385967+anonymes-axolotole@users.noreply.github.com>
2025-12-26 16:50:10 -0800

    I add GroundIntakePivotReal.java

--- src/main/java/frc/robot/subsystems/groundintake/GroundIntakePivotIOReal.java
+++ src/main/java/frc/robot/subsystems/groundintake/GroundIntakePivotIOReal.java
@@ -1,4 +1,19 @@
 package frc.robot.subsystems.groundintake;
 
-public class GroundIntakePivotIOReal {
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Constants;
+
+public class GroundIntakePivotIOReal implements GroundIntakePivotIO {
+
+    TalonFX groundIntakePivotMotor;
+    TalonFXConfiguration groundIntakeMotorPivotConfig;
+
+    public GroundIntakePivotIOReal() {
+
+        groundIntakePivotMotor = new TalonFX(Constants.G);
+
+    }
+
 }
